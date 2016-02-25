1400ab96d9ccb6d34f27f1e0c432ab7436c80e76
Pierre Oblin <pierre.oblin@orange.com>
2016-02-25 14:48:43 +0100

    switch stemcell builder concourse base image see https://hub.docker.com/r/bosh/os-image-stemcell-builder/tags/ modify global stemcells pipeline: - add cloustack stemcell - use concourse variable to externalize bosh git repo (to build on clones of cloudfoundry/bosh) fix pipeline modify osimage pipeline: - use concourse variable to externalize bosh git repo (to build on clones of cloudfoundry/bosh) modify osimage_new pipeline: - use concourse variable to externalize bosh git repo (to build on clones of cloudfoundry/bosh) modify osimage_new pipeline: - use concourse variable to externalize bosh git repo (to build on clones of cloudfoundry/bosh) fix cloudstack stemcell pipeline to match concourse outputs syntax fix cloudstack stemcell pipeline to match concourse outputs syntax fix task fix task - optimize disk usage by removing intermediate qcow/raw/vhd - generate tar zipped image instead of bzip2 rollback tar => bzip2

--- stemcell_builder/stages/prepare_vhdx_image_stemcell/apply.sh
+++ stemcell_builder/stages/prepare_vhdx_image_stemcell/apply.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 #
 
-
 set -e
 
 base_dir=$(readlink -nf $(dirname $0)/../..)
@@ -9,10 +8,14 @@ source $base_dir/lib/prelude_apply.bash
 
 rm -f $work/root.vhd
 rm -f $work/0.vhd 
+rm -f $work/0.raw
+rm -f $work/0.qcow2 
 
 # targeting xenserver vhd acceptable format. use img => qcow2 => raw
 qemu-img convert -c -O qcow2 $work/${stemcell_image_name} $work/0.qcow2
 qemu-img convert -O raw $work/0.qcow2 $work/0.raw
+rm -f $work/0.qcow2 
+
 
 #add faketime
 sudo apt-get install -y faketime
@@ -21,6 +24,9 @@ sudo apt-get install -y faketime
 #vhd-utils does only raw => fixed, or fixed => dynamic. chaining 2 conversions
 vhd-util convert -i $work/0.raw -s 0 -t 1  -o $work/0.vhd 
 faketime '2010-01-01' vhd-util convert -i $work/0.vhd -s 1 -t 2  -o $work/root.vhd 
+
+rm -f $work/0.vhd
+rm -f $work/0.raw
 
 #Verification: 
 vhd-util check -n $work/root.vhd 
