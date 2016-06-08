12075666bb05d647560354b3a667ec11e7397a82
Pierre Oblin <pierre.oblin@orange.com>
2016-06-08 20:20:29 +0200

    cloudstack stemcell manual rebase

--- bosh-stemcell/spec/support/stemcell_centos_rhel_7_shared_examples.rb
+++ bosh-stemcell/spec/support/stemcell_centos_rhel_7_shared_examples.rb
@@ -87,6 +87,9 @@ shared_examples_for 'a CentOS 7 or RHEL 7 stemcell' do
     exclude_on_vsphere: true,
     exclude_on_warden: true,
     exclude_on_openstack: true,
+    exclude_on_cloudstack: true,
+
+
   } do
     describe file('/etc/sysconfig/network') do
       it { should be_file }
@@ -126,6 +129,8 @@ shared_examples_for 'a CentOS 7 or RHEL 7 stemcell' do
     exclude_on_vsphere: true,
     exclude_on_warden: true,
     exclude_on_azure: true,
+    exclude_on_cloudstack: true,
+
   } do
     describe file('/var/vcap/bosh/agent.json') do
       it { should be_valid_json_file }
@@ -140,6 +145,8 @@ shared_examples_for 'a CentOS 7 or RHEL 7 stemcell' do
     exclude_on_openstack: true,
     exclude_on_warden: true,
     exclude_on_azure: true,
+    exclude_on_cloudstack: true,
+
    } do
     describe file('/var/vcap/bosh/agent.json') do
       it { should be_valid_json_file }
@@ -154,6 +161,8 @@ shared_examples_for 'a CentOS 7 or RHEL 7 stemcell' do
     exclude_on_vsphere: true,
     exclude_on_warden: true,
     exclude_on_openstack: true,
+    exclude_on_cloudstack: true,
+
   } do
     describe file('/var/vcap/bosh/agent.json') do
       it { should be_valid_json_file }
